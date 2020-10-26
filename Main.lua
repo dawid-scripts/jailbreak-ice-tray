2b63f7f89c84208bbec4066d503954e1c001fd5d
piglex9 <66374743+piglex9@users.noreply.github.com>
2020-10-26 02:40:50 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 getgenv().use_early_access = false
-if getgenv().use_early_access then
+if getgenv().use_early_access == true then
   local hash; do
     getgenv().use_early_access.network:FireServer("early_access called")
     if not "early_access called" then
