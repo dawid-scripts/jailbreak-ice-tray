f1981ac288b8669333ec0666cac6739dac827a75
piglex9 <66374743+piglex9@users.noreply.github.com>
2020-11-04 16:57:49 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,10 +1,3 @@
-local function load(...)
-    local args = {...}
-    local c = {}
-    c.load = args.load or false
-    if type(c.load):match("table") then
-        assert(c.load or not c.load, "not loading")
-    end
+if getfenv().script ~= "LocalScript" then
+    return "stop cheating"    
 end
-
-load()
